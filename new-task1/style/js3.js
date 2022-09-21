7e2a12b690a222f3e1caba1903c65cb6f0cb0f6c
Faten Hashim <fatenhashim73s@gmail.com>
2022-09-21 19:58:46 +0300

    NewTask1

---
+++ new-task1/style/js3.js
@@ -0,0 +1,10 @@
+let member = {
+    name: "Ahmed",
+    age: 20,
+    country: "Iraq"
+}
+console.log(member.name);
+console.log(member.age);
+console.log(member.country);
+console.log("_".repeat(20));
+console.log(("Name:" + (member.name)) + (", Age:" + (member.age)) + (", Country:" + (member.country)));
