e31cd40e3a30b8fbacceba16bab8fc949c0b5c9b
Faten Hashim <fatenhashim73s@gmail.com>
2022-09-22 10:14:09 +0300

    New Task2

---
+++ new-task2/style/js3.js
@@ -0,0 +1,21 @@
+let flowers = {
+    nameFlower: "jasmine",
+    colorFlower: {
+        one: "white",
+        tow: "yellow",
+        three: "pink",
+    },
+    homeFlower: {
+        country1: "china",
+        country2: "india",
+        country3: "egypt",
+    },
+    seasonFlower: {
+        first: "spring",
+        second: "summer"
+    },
+};
+console.log(flowers.nameFlower);
+console.log(flowers.colorFlower);
+console.log(flowers.homeFlower);
+console.log(flowers.seasonFlower);
